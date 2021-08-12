9e50d3f153c11eaf405babc2c742fcf7421c0190
liliia-r <54743392+liliia-r@users.noreply.github.com>
2021-08-12 14:27:50 +0300

    feat: reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let result = String(n)
+        .split("")
+        .filter((num) => num !== "-")
+        .reverse()
+        .filter((el) => n[0] !== "0")
+        .join("");
+    return result;
+};
